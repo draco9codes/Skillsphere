4fb065a07a4133e9289028d5a349148b0d75cf76
draco9codes <shivamkmar16385@gmail.com>
2026-01-05 15:06:45 +0530

    Hello World using card component!

---
+++ frontend/src/components/card/Card.tsx
@@ -0,0 +1,15 @@
+import "./Card.css";
+
+function Card({ title = "", image = "", description = "" }) {
+  return (
+    <div className="card">
+      {image && <img src={image} alt={title} className="card-image" />}
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Card from "../components/card/Card";
+
+function Home() {
+  return (
+    <>
+      <Card
+        title="Display Card"
+        image="frontend\src\assets\react.svg"
+        description="HELLO WORLD!"
+      />
+    </>
+  );
+}
+
+export default Home;
